ff71981524141b18f0089e872fc2ca7c5b8a2ebb
augustinlss <augustin.zhlass@gmail.com>
2025-07-24 00:36:30 +0200

    finished room creation logic

--- pkg/handlers.go
+++ pkg/handlers.go
@@ -1,18 +1,55 @@
 package pkg
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
-func handleCreateRoom(w http.ResponseWriter) (bool, error) {
+type RoomCreationRequest struct {
+	roomName string
+}
+
+func handleCreateRoom(w http.ResponseWriter, r *http.Request) {
+	var roomCreationRequest RoomCreationRequest
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	roomID, err := GenerateRandomToken(32)
 
 	if err != nil {
-		return false, err
+		http.Error(w, "Internal server error. Unable to generate room token...", http.StatusInternalServerError)
+		return
 	}
 
+	err = json.NewDecoder(r.Body).Decode(roomCreationRequest)
+
+	if err != nil {
+		http.Error(w, "Internal server error. Unable to read reqeust body...", http.StatusInternalServerError)
+	}
+
+	defer r.Body.Close()
+
+	roomName := roomCreationRequest.roomName
+
 	roomsLock.Lock()
-	rooms[roomID] = &Room{clients: make(map[*Client]bool), roomID: "123", roomName: "name"}
+	if _, exists := rooms[roomID]; exists {
+		log.Printf("Generated duplicate room id. Retrying...")
+		roomsLock.Unlock()
+		handleCreateRoom(w, r)
+		return
+	}
+
+	newRoom := NewRoom(roomID, roomName)
+	rooms[roomID] = newRoom
 	roomsLock.Unlock()
 
+	log.Printf("Successfuly created room %d", roomID)
+
+	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(roomID))
-	return true, nil
 }
