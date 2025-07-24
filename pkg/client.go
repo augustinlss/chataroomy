5d8f2e943d9972095b32d6f13b8f8a6dac9980ff
augustinlss <augustin.zhlass@gmail.com>
2025-07-24 16:37:06 +0200

    added fields to client struct

--- pkg/client.go
+++ pkg/client.go
@@ -3,6 +3,8 @@ package pkg
 import "github.com/gorilla/websocket"
 
 type Client struct {
-	conn *websocket.Conn
-	send chan []byte
+	conn     *websocket.Conn
+	send     chan []byte
+	username string
+	roomID   string
 }
