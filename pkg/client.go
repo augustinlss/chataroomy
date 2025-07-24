f8808bdeba924e4488870cd420871ec2409c1d42
augustinlss <augustin.zhlass@gmail.com>
2025-07-24 23:26:25 +0200

    implemented room joining and write pump for client

--- pkg/client.go
+++ pkg/client.go
@@ -1,10 +1,70 @@
 package pkg
 
-import "github.com/gorilla/websocket"
+import (
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
 	conn     *websocket.Conn
 	send     chan []byte
 	username string
 	roomID   string
+	room     *Room
+	userID   string
+	isClosed bool
+	mu       sync.Mutex
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(50 * time.Second)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.mu.Lock()
+			if c.isClosed {
+				c.mu.Unlock()
+				return
+			}
+			c.conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				c.isClosed = true
+				c.mu.Unlock()
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				c.isClosed = true
+				c.mu.Unlock()
+				return
+			}
+
+			w.Write(message)
+
+		case <-ticker.C:
+			c.mu.Lock()
+			if c.isClosed {
+				c.mu.Unlock()
+				return
+			}
+			c.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				c.isClosed = true
+				c.mu.Lock()
+				return
+			}
+
+			c.mu.Unlock()
+		}
+	}
+
 }
--- pkg/handlers.go
+++ pkg/handlers.go
@@ -25,7 +25,7 @@ func handleCreateRoom(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = json.NewDecoder(r.Body).Decode(roomCreationRequest)
+	err = json.NewDecoder(r.Body).Decode(&roomCreationRequest)
 
 	if err != nil {
 		http.Error(w, "Internal server error. Unable to read reqeust body...", http.StatusInternalServerError)
@@ -47,8 +47,63 @@ func handleCreateRoom(w http.ResponseWriter, r *http.Request) {
 	rooms[roomID] = newRoom
 	roomsLock.Unlock()
 
-	log.Printf("Successfuly created room %d", roomID)
+	log.Printf("Successfuly created room %s", roomID)
 
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(roomID))
+}
+
+func handleJoinRoom(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	queryParams := r.URL.Query()
+
+	roomID := queryParams.Get("roomID")
+	username := queryParams.Get("username")
+
+	roomsLock.RLock()
+	room, exists := rooms[roomID]
+	roomsLock.RUnlock()
+
+	if !exists {
+		log.Printf("Failed to find room with id %s.", roomID)
+		http.Error(w, "Room not found...", http.StatusNotFound)
+		roomsLock.RUnlock()
+		return
+	}
+
+	if username == "" {
+		log.Printf("No username.")
+		http.Error(w, "Invalid request. Username not provided.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Printf("Problem upgrading http connection for room %s: %v", roomID, err)
+		http.Error(w, "Unable to upgrade connection...", http.StatusInternalServerError)
+		return
+	}
+
+	userID, err := GenerateRandomToken(32)
+
+	if err != nil {
+		log.Printf("Error generating user id.")
+		http.Error(w, "Internal server error. Error generating user ID.", http.StatusInternalServerError)
+		conn.Close()
+		return
+	}
+
+	client := &Client{
+		username: username,
+		roomID:   roomID,
+		room:     room,
+		send:     make(chan []byte, 256),
+		userID:   userID,
+	}
+
 }
